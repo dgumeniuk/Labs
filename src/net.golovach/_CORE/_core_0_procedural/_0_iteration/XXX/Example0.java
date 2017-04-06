a3d69a8ac937e7e5c006cd452f0e751adc9657e5
Dmytro Gumeniuk <d.gumeniuk.javadev@gmail.com>
2017-04-07 02:19:56 +0300

    Class work

--- src/net.golovach/_CORE/_core_0_procedural/_0_iteration/XXX/Example0.java
+++ src/net.golovach/_CORE/_core_0_procedural/_0_iteration/XXX/Example0.java
@@ -1,13 +1,10 @@
 package net.golovach._CORE._core_0_procedural._0_iteration.XXX;
 
-/**
- * Created by dgumeniuk on 04.04.2017.
- */
 public class Example0 {
     public static void main(String[] args) {
-//        for (int k = 0; k < 10; k++) {
-//            System.out.println(k);
-//        }
+        for (int k = 0; k < 10; k++) {
+            System.out.println(k);
+        }
 
         int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         for (int i = array.length - 1; i >= 0; i--) {
--- src/net.golovach/_CORE/_core_0_procedural/_0_iteration/lab/array_inverter/ArrayInverter_q.java
+++ src/net.golovach/_CORE/_core_0_procedural/_0_iteration/lab/array_inverter/ArrayInverter_q.java
@@ -5,14 +5,30 @@ import java.util.Arrays;
 public class ArrayInverter_q {
     public static void main(String[] args) {
         int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        invert(array);
-        toString(array);
         System.out.println(Arrays.toString(array));
+        invert(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(new float[]{1f, 2.3f, 3, 5.6f, 6, 8.0f}));
+        System.out.println(3/6.1);
+        System.out.println(1.0 % 0.0);
+        switchNOTmpDemo();
+        toString(array);
         StringBuilder str = new StringBuilder();
         str.append(56);
         str.insert(2, 4);
         System.out.println(str);
         // TODO: 07.04.2017 delete some chars from string
+
+    }
+
+    private static void switchNOTmpDemo() {
+        int a = 2;
+        int b = 3;
+        System.out.println("a=" + a + " b=" + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a=" + a + " b=" + b);
     }
 
     private static void invert(int[] array) {
