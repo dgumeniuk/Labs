56aa058ba97e6ba76f90e27accd86d7f7ca6c6c1
Dmytro Gumeniuk <d.gumeniuk.javadev@gmail.com>
2017-04-06 23:10:38 +0300

    Initial commit

---
+++ src/net.golovach/_CORE/_core_0_procedural/_0_iteration/lab/array_inverter/ArrayInverter_q.java
@@ -0,0 +1,34 @@
+package net.golovach._CORE._core_0_procedural._0_iteration.lab.array_inverter;
+
+import java.util.Arrays;
+
+public class ArrayInverter_q {
+    public static void main(String[] args) {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        invert(array);
+//        toString(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+
+    public static void invert(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int tmp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = tmp;
+        }
+
+    }
+
+    public static void toString(int[] array){
+        for (int i=0; i<array.length; i++){
+            if(i==0){
+                System.out.print("[");
+            }else if(i==array.length-1){
+                System.out.println(array[i]+"]");
+                break;
+            }
+            System.out.print(array[i]+", ");
+        }
+    }
+}
